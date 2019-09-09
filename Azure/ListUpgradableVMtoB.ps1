e366bd68e5b3bd0256d18a1a77ee53f20bf874eb
mathwro <mathias@wrobeldk>
2019-09-09 16:22:57 +0200

    Changed check for series to check for available resizing options for specific VM

--- Azure/ListUpgradableVMtoB.ps1
+++ Azure/ListUpgradableVMtoB.ps1
@@ -13,10 +13,14 @@ ForEach ($sub in $subscriptionIDlist) {
     #Looping through VM's in current subscription
         $listOfVMs = (Get-AzVm)
         $listOfVMDisks = (Get-AzDisk)
+
+        #Looping through VM's in current subscription
         ForEach ($vm in $listOfVMs) {
-            if ($vm.HardwareProfile.VmSize.ToString() -notmatch "b") {
+            #Checking if already in B-series
+            if ($vm.HardwareProfile.VmSize.ToString() -notmatch "Standard_b") {
                 $disk = ($listOfVMDisks | Where-Object { $_.ManagedBy -eq $vm.Id })
-                if ($disk.Sku.Tier -eq "Premium") {
+                #Checking available resize options for b-series
+                if ((Get-AzVMSize -VMName $vm.Name -ResourceGroupName $vm.ResourceGroupName | Where-Object { $_.Name -match "Standard_b"})) {
                     $arrayOfVMs += New-Object -TypeName psobject -Property @{Name = $vm.Name;
                                                                             ResourceGroupName = $vm.ResourceGroupName;
                                                                             VmSize = $vm.HardwareProfile.VmSize;
@@ -29,5 +33,12 @@ ForEach ($sub in $subscriptionIDlist) {
         }
 }
 
+#Setting AzContext to starting point
 Set-AzContext $currentContext | Out-Null
+
+#Exporting to csv
+if ($export) {
+    $arrayOfVMs | Select-Object Name, ResourceGroupName, VmSize, DiskName, DiskSku, Subscription | Export-Csv -Path C:\Users\mathias.wrobel\Desktop\Export.csv -NoTypeInformation -Append -Delimiter ";"
+}
+
 $arrayOfVMs | Format-Table -Property Name, ResourceGroupName, VmSize, DiskName, DiskSku, Subscription
