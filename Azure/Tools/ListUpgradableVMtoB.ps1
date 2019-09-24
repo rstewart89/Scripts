6f42483f34173d71665d5f37d7319fda62b77915
mathwro <mathias@wrobeldk>
2019-09-24 12:02:58 +0200

    Updates

--- Azure/Tools/ListUpgradableVMtoB.ps1
+++ Azure/Tools/ListUpgradableVMtoB.ps1
@@ -1,6 +1,12 @@
 #Author: Mathias Wrobel - Innofactor
 #Script to list VM's that can be upgraded to series B available to that tenant.
 
+param (
+    [Boolean] $export = $false,
+    [Boolean] $notUpgradeable = $false
+)
+
+
 $currentContext = Get-AzContext
 $subscriptionIDlist = Get-AzSubscription
 
@@ -15,26 +21,41 @@ ForEach ($sub in $subscriptionIDlist) {
     Set-AzContext -SubscriptionId $sub.Id | Out-Null
 
     #Looping through VM's in current subscription
-        $listOfVMs = (Get-AzVm)
-        $listOfVMDisks = (Get-AzDisk)
+    $listOfVMs = (Get-AzVm)
+    $listOfVMDisks = (Get-AzDisk)
 
-        #Looping through VM's in current subscription
-        ForEach ($vm in $listOfVMs) {
-            #Checking if already in B-series
-            if ($vm.HardwareProfile.VmSize.ToString() -notmatch "Standard_b") {
-                $disk = ($listOfVMDisks | Where-Object { $_.ManagedBy -eq $vm.Id })
+    #Looping through VM's in current subscription
+    ForEach ($vm in $listOfVMs) {
+        #Checking if already in B-series
+        if ($vm.HardwareProfile.VmSize.ToString() -notmatch "Standard_b") {
+            $disk = ($listOfVMDisks | Where-Object { $_.ManagedBy -eq $vm.Id })
+
+            if ($notUpgradeable) {
                 #Checking available resize options for b-series
-                if ((Get-AzVMSize -VMName $vm.Name -ResourceGroupName $vm.ResourceGroupName | Where-Object { $_.Name -match "Standard_b"})) {
+                if (!(Get-AzVMSize -VMName $vm.Name -ResourceGroupName $vm.ResourceGroupName | Where-Object { $_.Name -match "Standard_b" })) {
                     $arrayOfVMs += New-Object -TypeName psobject -Property @{Name = $vm.Name;
-                                                                            ResourceGroupName = $vm.ResourceGroupName;
-                                                                            VmSize = $vm.HardwareProfile.VmSize;
-                                                                            Subscription = $sub.Name;
-                                                                            DiskName = $disk.Name;
-                                                                            DiskSku = $disk.Sku.Name
-                                                                            }
+                        ResourceGroupName                                         = $vm.ResourceGroupName;
+                        VmSize                                                    = $vm.HardwareProfile.VmSize;
+                        Subscription                                              = $sub.Name;
+                        DiskName                                                  = $disk.Name;
+                        DiskSku                                                   = $disk.Sku.Name
+                    }
+                }
+            }
+            else {
+                #Checking available resize options for b-series
+                if ((Get-AzVMSize -VMName $vm.Name -ResourceGroupName $vm.ResourceGroupName | Where-Object { $_.Name -match "Standard_b" })) {
+                    $arrayOfVMs += New-Object -TypeName psobject -Property @{Name = $vm.Name;
+                        ResourceGroupName                                         = $vm.ResourceGroupName;
+                        VmSize                                                    = $vm.HardwareProfile.VmSize;
+                        Subscription                                              = $sub.Name;
+                        DiskName                                                  = $disk.Name;
+                        DiskSku                                                   = $disk.Sku.Name
+                    }
                 }
             }
         }
+    }
 }
 
 #Setting AzContext to starting point
@@ -42,7 +63,7 @@ Set-AzContext $currentContext | Out-Null
 
 #Exporting to csv
 if ($export) {
-    $arrayOfVMs | Select-Object Name, ResourceGroupName, VmSize, DiskName, DiskSku, Subscription | Export-Csv -Path C:\Users\mathias.wrobel\Desktop\Export.csv -NoTypeInformation -Append -Delimiter ";"
+    $arrayOfVMs | Select-Object Name, ResourceGroupName, VmSize, DiskName, DiskSku, Subscription | Export-Csv -Path C:\Users\mathias.wrobel\Desktop\VMlistSeries.csv -NoTypeInformation -Append -Delimiter ";"
 }
 
 $arrayOfVMs | Format-Table -Property Name, ResourceGroupName, Subscription, VmSize, DiskName, DiskSku
